ede4a4f9c65a889280c507d0fccd25c0b0f87db2
ricardobuquet <ricardo@productiv.com>
2022-11-21 09:44:19 -0800

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@babel/eslint-parser"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": "off",
+    "vue/multi-word-component-names": "off"
   },
-  overrides: [
-    {
-      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+  overrides: [{
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    env: {
+      jest: true
+    }
+  }]
 };
